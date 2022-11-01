2e291373f5c3044ec423f72a4b0443590ebac099
paulAlexSerban <paul.alex.serban@gmail.com>
2022-11-01 10:52:12 +0200

    feat(fe-build): implemnt base frontend build

--- frontend/js-component-library/source/layout/header/header.js
+++ frontend/js-component-library/source/layout/header/header.js
@@ -1,12 +1,10 @@
-import './header.scss';
-
 const headerStructure = () => {
 	class Header {
 		constructor(element) {
 			this.element = element;
 			this.init()
 		}
-	
+
 		init() {
 			console.log("class Header - is loaded", this.element);
 		}
--- frontend/js-component-library/source/layout/section/section.js
+++ frontend/js-component-library/source/layout/section/section.js
@@ -1,13 +1,10 @@
-import './section.scss';
-
-
 const sectionStructure = () => {
 	class Section {
 		constructor(element) {
 			this.element = element;
 			this.init()
 		}
-	
+
 		init() {
 			console.log("class section - is loaded", this.element);
 		}
