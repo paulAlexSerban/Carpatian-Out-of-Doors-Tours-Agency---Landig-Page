77957088302fd58a5ccc64d31edcb7b5b77bc512
paulAlexSerban <paul.alex.serban@gmail.com>
2023-04-02 14:28:31 +0300

    ci: files not getting found

--- frontend/landing-ssg/scripts/package.bash
+++ frontend/landing-ssg/scripts/package.bash
@@ -5,9 +5,13 @@ cd "$(dirname "$0")" || exit
 source ../config.env
 
 echo -e "📦  Package Landing SSG Website"
+
 mkdir -p ../../../package/landing-ssg
+
+# Copy the output of the build into the package folder
 cp -rfv ../dist/* ../../../package/landing-ssg
-touch ../../../package/landing-ssg/.nojekyll
-echo -e "$GREEN [ info ] $NC .nojekyll created and added. $NC"
-echo "Create a CNAME file"
-echo 'https://paulalexserban.github.io/' > ../../../package/landing-ssg/CNAME
+
+# Make the package folder read-only
+chmod -R a-w ../../../package/landing-ssg
+# Make the package folder executable
+chmod -R a+x ../../../package/landing-ssg
